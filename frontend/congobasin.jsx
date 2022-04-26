85bf93bbd67663f11b419bab72c9b23d3855240f
peterkimaa <peterkimaa@LAPTOP-DGE440JA.localdomain>
2022-04-26 16:03:50 -0400

    user model/controller working

--- frontend/congobasin.jsx
+++ frontend/congobasin.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>It's working!</h1>, root);
+  ReactDOM.render(<h1>It's working for real!</h1>, root);
 });
